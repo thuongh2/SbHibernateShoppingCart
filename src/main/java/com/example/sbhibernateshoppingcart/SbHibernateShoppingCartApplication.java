0fa02b207e67fe324a7c0721a186af11954eb1d6
thuongh2 <vo.thuong01632985@gmail.com>
2022-03-02 21:07:14 +0700

    first commit

---
+++ src/main/java/com/example/sbhibernateshoppingcart/SbHibernateShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbhibernateshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHibernateShoppingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbHibernateShoppingCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbhibernateshoppingcart/SbHibernateShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbhibernateshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHibernateShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
